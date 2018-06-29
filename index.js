df0dca9c4430d14523c8772747a7c34aa4aac1d5
Charles Kim <cwkim3@gmail.com>
2018-06-29 00:23:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ function divide (x,y){return x / y}
 function inc(n){return ++n}
 function dec(n){return --n}
 function makeInt(n){return parseInt(n,10)}
+function preserveDecimal(n){return parseFloat(n)}
